0d785a692c83c2e8958b7737c71c3c03651f6fb6
Egor Fedorenko <68076283+h2sm@users.noreply.github.com>
2021-10-14 15:07:44 +0300

    implementing extended euclidean method

--- euclidean.py
+++ euclidean.py
@@ -1,5 +1,4 @@
-def first():
-    mas = returnTwoNumbers()
+def first(mas):
     a=mas[0]
     b=mas[1]
     while a != 0 and b != 0:
@@ -9,8 +8,17 @@ def first():
             b = b % a
     print(a + b)
 
-def second():
-    mas = returnTwoNumbers()
+def second(mas):
+    a=mas[0]
+    b=mas[1]
+    x, xx, y, yy = 1, 0, 0, 1
+    while b:
+        q = a // b
+        a, b = b, a % b
+        x, xx = xx, x - xx*q
+        y, yy = yy, y - yy*q
+    print( x, y, a)
+
 
 def returnTwoNumbers():
     print('Введите 2 числа')
@@ -22,8 +30,8 @@ print('Обычный евклид (1) или расширенный? (2)')
 choice = int(input())
 
 if choice == 1:
-    first()
+    first(returnTwoNumbers())
 elif choice == 2 :
-    second()
+    second(returnTwoNumbers())
 else:
     print('ок поняла')
